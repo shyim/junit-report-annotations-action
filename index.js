5346527fa0ce9c429289f30f9e7862d99d94e07a
Soner Sayakci <s.sayakci@shopware.com>
2020-05-07 23:06:07 +0200

    Add logging, strip file prefix

--- index.js
+++ index.js
@@ -3,63 +3,74 @@ const github = require('@actions/github');
 const glob = require('@actions/glob');
 const fs = require('fs');
 var parseString = require('xml2js').parseStringPromise;
+const stripPrefix = process.cwd() + '/';
 
 (async () => {
-    const path = core.getInput('path');
-    const accessToken = core.getInput('access-token');
-    
-    const globber = await glob.create(path, {followSymbolicLinks: false});
-    let annotations = [];
+    try {
+        const path = core.getInput('path');
+        const accessToken = core.getInput('access-token');
+        
+        const globber = await glob.create(path, {followSymbolicLinks: false});
+        let annotations = [];
 
-    for await (const file of globber.globGenerator()) {
-        const data = await fs.promises.readFile(file);
-        var json = await parseString(data);
-    
-        if (json.testsuites === undefined) {
-            continue;
-        }
-
-        for (let row of json.testsuites.testsuite) {
-            if (row.testcase !== undefined) {
-                row.testsuite = [row];
+        for await (const file of globber.globGenerator()) {
+            const data = await fs.promises.readFile(file);
+            var json = await parseString(data);
+        
+            if (json.testsuites === undefined) {
+                continue;
             }
 
-            for (let testsuite of row.testsuite) {
-                if (testsuite['$']['errors'] !== '0' || testsuite['$']['failures'] !== '0') {
-                    for (let testCase of testsuite.testcase) {
-                        if (testCase.failure) {
-                            annotations.push({
-                                path: testCase['$'].file,
-                                start_line: testCase['$'].line || '0',
-                                end_line: testCase['$'].line || '0',
-                                start_column: 0,
-                                end_column: 0,
-                                annotation_level: 'failure',
-                                message: testCase.failure[0]['_'],
-                              });
+            for (let row of json.testsuites.testsuite) {
+                if (row.testcase !== undefined) {
+                    row.testsuite = [row];
+                }
+
+                for (let testsuite of row.testsuite) {
+                    if (testsuite['$']['errors'] !== '0' || testsuite['$']['failures'] !== '0') {
+                        for (let testCase of testsuite.testcase) {
+                            if (testCase.failure) {
+                                annotations.push({
+                                    path: testCase['$'].file.replace(stripPrefix, ''),
+                                    start_line: testCase['$'].line || '0',
+                                    end_line: testCase['$'].line || '0',
+                                    start_column: 0,
+                                    end_column: 0,
+                                    annotation_level: 'failure',
+                                    message: testCase.failure[0]['_'],
+                                });
+                            }
                         }
                     }
                 }
             }
-        }
 
-        const octokit = new github.GitHub(accessToken);
-        const req = {
-        ...github.context.repo,
-        ref: github.context.sha
-        }
-        const res = await octokit.checks.listForRef(req);
-        const check_run_id = res.data.check_runs.filter(check => check.name === 'build')[0].id
+            console.log(annotations);
 
-        const update_req = {
-            ...github.context.repo,
-            check_run_id,
-            output: {
-                title: "Junit Results",
-                summary: `jUnit Results`,
-                annotations: annotations
+            if (annotations.length === 0) {
+                return;
             }
+
+            const octokit = new github.GitHub(accessToken);
+            const req = {
+                ...github.context.repo,
+                ref: github.context.sha
+            }
+            const res = await octokit.checks.listForRef(req);
+            const check_run_id = res.data.check_runs.filter(check => check.name === 'build')[0].id
+
+            const update_req = {
+                ...github.context.repo,
+                check_run_id,
+                output: {
+                    title: "Junit Results",
+                    summary: `jUnit Results`,
+                    annotations: annotations
+                }
+            }
+            await octokit.checks.update(update_req);
         }
-        await octokit.checks.update(update_req);
+    } catch(error) {
+        core.setFailed(error.message);
     }
 })();
